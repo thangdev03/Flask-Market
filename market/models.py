a712aa40e35611259ba1bb4073556b23df7da022
thangdev03 <thangdev03@gmail.com>
2023-09-24 18:34:13 +0700

    add models relationships

--- market/models.py
+++ market/models.py
@@ -1,12 +1,23 @@
 from market import db
 
 # MODELS FOR DATABASE
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(60), nullable=False)
+    budget = db.Column(db.Integer, nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True) 
+    
+    def __repr__(self):
+        return f'User {self.username}'
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     barcode = db.Column(db.String(12), nullable=False, unique=True)
     price = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(1024), nullable=False)
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     # Define how Item will be showed in Terminal item.query.all()
     def __repr__(self):
