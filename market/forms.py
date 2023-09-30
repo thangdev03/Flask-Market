5f50376cd7e2b07d4f1f07fd954549ff0ca805a0
thangdev03 <thangdev03@gmail.com>
2023-09-30 13:04:18 +0700

    add login page

--- market/forms.py
+++ market/forms.py
@@ -18,3 +18,8 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1','Passwords do not match, please confirm again!'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+class LoginForm(FlaskForm):
+    username = StringField(label='Username:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Sign In')
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash
 from market import app, db
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 
 # ROUTES TO PAGES
 @app.route("/")
@@ -32,3 +32,8 @@ def register_page():
             flash(f'Error in creating user: {err_msg}', 'danger')
 
     return render_template('register.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
